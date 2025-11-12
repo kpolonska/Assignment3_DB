355a34962c732a6d52cb1b83f4cf073ee52f0af3
Kristina Polonska <kpolonska@kse.org.ua>
2025-11-12 19:49:21 +0200

    finalise querie

--- stg_query.sql
+++ stg_query.sql
@@ -63,4 +63,18 @@ select doctor_id, doctor_name, doctor_speciality, years_of_experience, doctor_ge
 from strong-harbor-474616-p4.assignment3_kyp.doctors_stg;
 
 create or replace table strong-harbor-474616-p4.assignment3_kyp.treatment_fact as 
+select ir.record_id as treatment_id, 
+ir.patient_id, 
+ir.illness_id, 
+me.med_id as medication_id, 
+me.prescribed_by as prescriber_doctor_id, 
+p.doctor_id as doctor_curator_id, 
+(me.med_price_usd * p.days_in_hospital) as calculated_cost,
+p.days_in_hospital as treatment_duration, 
+me.med_price_usd, me.med_dosage_mg, ir.severity as illness_severity
+from strong-harbor-474616-p4.assignment3_kyp.illness_records_stg ir
+left join strong-harbor-474616-p4.assignment3_kyp.medication_stg me 
+on ir.illness_id = me.illness_id
+left join strong-harbor-474616-p4.assignment3_kyp.patients_stg p
+on ir.patient_id = p.patient_id
  
